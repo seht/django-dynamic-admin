f78c3cc3853bee1def5f251ff6fb1e384d82486f
seht <seht@hyx.net>
2019-10-16 20:19:01 +0100

    django app, migrations

---
+++ dynamicadmin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DynamicadminConfig(AppConfig):
+    name = 'dynamicadmin'
--- dynamicadmin/bundle/admin.py
+++ dynamicadmin/bundle/admin.py
@@ -11,10 +11,6 @@ from dynamicadmin.entity.models import BundleEntity
 from .models import CharField, TextField, TaxonomyDictionaryField, DateTimeField, URLField
 
 
-# import reversion
-# from reversion.admin import VersionAdmin
-
-
 class BundleAdmin(admin.ModelAdmin):
 
     class BundleTabularInline(admin.TabularInline):
@@ -45,28 +41,50 @@ class BundleAdmin(admin.ModelAdmin):
     ]
 
 
-def register_content_models(bundle_model, app_label, model_admin=admin.ModelAdmin, base=BundleEntity, **kwargs):
+def get_bundle_objects(bundle_model):
+    return bundle_model.objects.all()
+
+
+def register_bundle_model(bundle_model, model_admin=BundleAdmin):
+    if not admin.site.is_registered(bundle_model):
+        admin.site.register(bundle_model, model_admin)
+
+
+def unregister_bundle_model(bundle_model):
+    if admin.site.is_registered(bundle_model):
+        admin.site.unregister(bundle_model)
+
+
+# def reload_bundle_models(models=list()):
+#     for model in models:
+#         unregister_bundle_model(model)
+#         register_bundle_model(model)
+#     reload(import_module(settings.ROOT_URLCONF))
+#     clear_url_caches()
+
+
+def get_dynamic_models(app_label):
+    return apps.get_app_config(app_label).get_models()
+
+
+def register_dynamic_model(bundle_model_object, model_admin=admin.ModelAdmin, **kwargs):
+    dynamic_model = bundle_model_object.create_django_model(**kwargs)
+    if not admin.site.is_registered(dynamic_model):
+        admin.site.register(dynamic_model, model_admin)
+
+
+def register_dynamic_models(bundle_model, app_label, model_admin=admin.ModelAdmin, base=BundleEntity, **kwargs):
     kwargs['app_label'] = app_label
     kwargs['base'] = base
-    bundle_models = bundle_model.objects.all()
-    try:
-        for bundle_content_model in bundle_models:
-            content_model = bundle_content_model.create_django_model(**kwargs)
-            # @todo Support reversion.
-            # reversion.register(content_model)
-            if not admin.site.is_registered(content_model):
-                admin.site.register(content_model, model_admin)
-    except ProgrammingError:
-        pass
-    # @todo Optional/custom permissions.
-    create_permissions(apps.get_app_config(app_label), verbosity=2)
+    bundle_models_objects = get_bundle_objects(bundle_model)
+    for bundle_model_object in bundle_models_objects:
+        register_dynamic_model(bundle_model_object, model_admin=model_admin, **kwargs)
 
 
-def unregister_content_models(app_label):
-    registry_models = apps.get_app_config(app_label).get_models()
-    for model in registry_models:
-        admin.site.unregister(model)
-    # @see django.apps.registry.register_model
+# @see django.apps.registry.register_model
+def unregister_dynamic_models(app_label):
+    for dynamic_model in get_dynamic_models(app_label):
+        admin.site.unregister(dynamic_model)
     app_models = apps.all_models[app_label]
     for model_name in app_models.copy():
         model = apps.get_model(app_label, model_name)
@@ -76,19 +94,5 @@ def unregister_content_models(app_label):
     apps.clear_cache()
 
 
-def register_bundle_model(bundle_model, model_admin=BundleAdmin):
-    if not admin.site.is_registered(bundle_model):
-        admin.site.register(bundle_model, model_admin)
-
-
-def unregister_bundle_model(model):
-    unregister_content_models(model.CONTENT_BUNDLE)
-
-
-def reload_bundle_models(models=list()):
-    for model in models:
-        unregister_bundle_model(model)
-        register_bundle_model(model)
-    # apps.clear_cache()
-    reload(import_module(settings.ROOT_URLCONF))
-    clear_url_caches()
+def create_dynamic_model_permissions(verbosity=2):
+    create_permissions(apps.get_app_config(app_label), verbosity=verbosity)
