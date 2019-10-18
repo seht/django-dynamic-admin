31d22db6a267d36ab77c2dd66a74e40c25afa9fd
seht <seht@hyx.net>
2019-10-18 13:50:52 +0100

    repack

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-dynamic-admin',
-    version='0.2.0.3-alpha',
+    version='0.2.1-alpha1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
