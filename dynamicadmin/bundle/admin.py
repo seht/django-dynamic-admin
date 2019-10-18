fb138825b58c57a4c98f16293241163ae7e9289b
seht <seht@hyx.net>
2019-10-18 13:24:27 +0100

    plymorphic mixin...

--- dynamicadmin/bundle/admin.py
+++ dynamicadmin/bundle/admin.py
@@ -6,7 +6,7 @@ from django.contrib.auth.management import create_permissions
 # from django.conf import settings
 # from django.urls.base import clear_url_caches
 # from django.utils.module_loading import import_module
-from polymorphic.admin import PolymorphicParentModelAdmin, StackedPolymorphicInline
+from polymorphic.admin import PolymorphicParentModelAdmin, StackedPolymorphicInline, PolymorphicInlineSupportMixin
 from dynamicadmin.entity.models import BundleEntity
 from .models import get_dynamic_models, get_bundle_objects
 from .models import Field, CharField, TextField, TaxonomyDictionaryField, DateTimeField, URLField
@@ -41,7 +41,7 @@ class FieldAdminInline(StackedPolymorphicInline):
     # classes = ['collapse']
 
 
-class BundleAdmin(PolymorphicParentModelAdmin):
+class BundleAdmin(PolymorphicParentModelAdmin, PolymorphicInlineSupportMixin):
     child_models = (Field,)
     inlines = (FieldAdminInline,)
 
