f81c85cb2b61c91c31bc628faa77bd4cf68a01a6
seht <seht@hyx.net>
2019-10-19 14:53:08 +0100

    tag v0.3.0-alpha.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-dynamic-admin',
-    version='0.2.2-alpha.1',
+    version='0.3.0-alpha.1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
